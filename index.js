12ee59017a8cb410964018c95d429f2520474c7d
Guodaxia <1265986077@qq.com>
2024-04-24 22:46:40 +0800

    feat:todolist调用api交互功能基本实现

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let tab = 'all';
 // 定义接口api
 //   获取先全部数据
 const getTodos = () => {
@@ -47,53 +48,32 @@ const addTodoDomToHtml = (title, id, status) => {
   todoItemDom.innerHTML = liWithSpan;
   todoListDom.appendChild(todoItemDom);
   todoItemDom.classList.add('todo-li');
+  todoItemDom.setAttribute('id', 'li')
   todoItemDom.dataset.id = id;
-  addDeleteButtonTodo(todoItemDom,title, id);
-  addEditButtonTodo(todoItemDom,title, id);
+  addDeleteButtonTodo(todoItemDom, title, id);
+  addEditButtonTodo(todoItemDom, title, id);
   const checkbox = document.createElement('input')
   checkbox.classList.add('div1')
   checkbox.type = 'checkbox'
-  if (status===1){
+  if (status === 1) {
     checkbox.checked = true;
-    todoItemDom.dataset.status=1
-  }else{
-    checkbox.checked=false
-    todoItemDom.dataset.status=0
+    todoItemDom.dataset.status = 1
+  } else {
+    checkbox.checked = false
+    todoItemDom.dataset.status = 0
   }
   todoItemDom.prepend(checkbox)
-
   checkbox.addEventListener('click', () => {
     const status = checkbox.checked ? 1 : 0;
     updateTodoStatus(id, status)
       .then(() => {
-        // todoItemDom.classList.toggle('status', status === 1);
+
         todoItemDom.dataset.status = status;
         console.log('Todo updated successfully:');
-        let lis = document.querySelectorAll('li');
-        console.log(lis.length);
-        console.log(initLength);
-        // lis.forEach(lis=>{
-        //   if(lis.style.display='block'){
-        //     Alltodo()
-        //   }else if(lis.style.display='none')
-        //   {
-        //  if(status===0){
-        //   completedtodo()
-        //  }else if (status===1){
-        //   Activetodo()
-        //  }
-        // }
-        // })
-        // const ALL=document.getElementById('reset')
-        // const expandedButton = document.querySelector('button[aria-expanded="true"]');
-        // if (ALL.style.contain(aria-expanded)){
-        //   Alltodo()
-        // }else
-       
-    
+        getTodosAndRender(tab)
       })
 
-})
+  })
 
 }
 
@@ -104,48 +84,40 @@ const removeTodoDomItem = (domElement) => {
 
 //更新 DOM 中的 todo 标题
 const updateTodoDomTitle = (domElement, newTitle) => {
-  domElement.innerHTML =`<span>${newTitle}</span>`;
+  domElement.innerHTML = `<span>${newTitle}</span>`;
 };
-
-let initLength = 0;
-
-//保存数据长度
-const getLength = (type) => {
-  if (type === 'add') {
-    initLength += 1;
-  } else if (type === 'delete') {
-    initLength -= 1;
-  }
-  // if(type==='Active'){
-  //   initLength-=1
-  // }else if (type==='complted'){
-  //   initLength-=1
-  // }
-  const remain = document.getElementById("remain");
-  const remains = (`${initLength}tasks remaining`)
-  remain.innerHTML = remains
-
-}
-
 
 // 添加完数据清除 input文本框
 const clearInput = () => {
   document.getElementById("todo-input").value = "";
 };
-let maxId = 0
 // 1、拉取数据,渲染数据
-const getTodosAndRender = () => {
+const getTodosAndRender = (type = 'all') => {
   getTodos()
     .then((res) => {
       const todoDatas = res["data"] || [];
-      initLength = todoDatas.length;
-      todoDatas.forEach((item) => {
-        addTodoDomToHtml(item.title, item.id,item.status);
-        maxId = item.id;
+      const tasks = todoDatas.filter((i) => {
+        switch (type) {
+          case 'all':
+            return todoDatas;
+          case 'active':
+            return i.status == 0
+          case 'completed':
+            return i.status == 1;
+          default:
+            return todoDatas;
+        }
       });
+      const todoListDom = document.getElementById("todo-list");
+      todoListDom.innerHTML = '';
+      tasks.forEach((item) => {
+        addTodoDomToHtml(item.title, item.id, item.status);
 
+      });
       const remain = document.getElementById("remain");
-      const remains = (`${initLength}tasks remaining`)
+      const maxlength = tasks.length;
+      const tasksortask = maxlength > 1 ? 'tasks' : 'task'
+      const remains = (`${tasks.length}${tasksortask} ${tab === 'active' ? "activing" : tab === 'completed' ? 'completed' : 'remaining'}`)
       remain.innerHTML = remains
 
     })
@@ -159,11 +131,11 @@ const getTodosAndRender = () => {
 const addTodo = () => {
   const title = (document.getElementById("todo-input").value || "").trim();
   if (title !== "") {
+    getTodosAndRender(tab)
     createTodo({ title, })
       .then((res) => {
         if (res.data && res.data.title) addTodoDomToHtml(res.data.title, ++maxId);
         clearInput();
-        getLength('add');
       })
       .catch((error) => {
         console.error(error.message);
@@ -172,7 +144,7 @@ const addTodo = () => {
 };
 
 // 3.添加删除按钮并绑定点击事件
-const addDeleteButtonTodo = (todoItemDom,title, id,data) => {
+const addDeleteButtonTodo = (todoItemDom, title, id, data) => {
   const deleteButton = document.createElement('button');
   //增加div标签用来包含编辑和删除按钮
   const delandedit = document.createElement('div')
@@ -185,8 +157,9 @@ const addDeleteButtonTodo = (todoItemDom,title, id,data) => {
   //通过id删除数据
   deleteButton.addEventListener('click', () => {
     deleteTodo(id)
-      .then(() => removeTodoDomItem(todoItemDom)).then(() => getLength('delete'))
-      .catch((error) => console.error(error.message));
+    .then(() => removeTodoDomItem(todoItemDom))
+    .catch((error) => console.error(error.message));
+    getTodosAndRender(tab)
   });
 
 };
@@ -194,76 +167,55 @@ const addDeleteButtonTodo = (todoItemDom,title, id,data) => {
 // 4.checkbox的点击status状态更新
 const updateTodoStatus = (id, status) => {
   return updateTodo(id, { status })
-    .then((res) => {
+      .then((res) => {
       if (status === 1) {
         console.log(`Todo with ID ${id} is completed.`);
-      } 
+      }
       return res.data;
-    })
+      })
     .catch((error) => {
       console.error(error.message);
     });
 };
-// 4.添加取消和保存按钮触发时隐藏编辑和删除按钮
+// 添加取消和保存按钮触发时隐藏编辑和删除按钮
 const editInput = document.createElement('input');
-const cancelButton=document.createElement('button');
+const cancelButton = document.createElement('button');
 const saveButton = document.createElement('button');
-// const editButton=document.querySelectorAll('.edit')
-// const deleteButton=document.querySelectorAll('.delete')
-
 //5.编辑按钮触发事件
-const addEditButtonTodo = (todoItemDom,title) => {
+const addEditButtonTodo = (todoItemDom, title) => {
   const editButton = document.createElement('button');
   editButton.classList.add('edit');
   editButton.innerText = 'Edit';
-  todoItemDom.appendChild(editButton); 
+  todoItemDom.appendChild(editButton);
   const titleSpan = todoItemDom.querySelector('span');
-  editButton.addEventListener('click', () => toggleEditMode(todoItemDom,titleSpan)); 
+  editButton.addEventListener('click', () => toggleEditMode(todoItemDom, titleSpan));
 };
 
-
 //6.编辑功能 
-const toggleEditMode = (todoItemDom,titleSpan) => {
+const toggleEditMode = (todoItemDom, titleSpan) => {
   //创建修改文本的文本框
-  const deleteButton=todoItemDom.querySelector('.delete')
+  const deleteButton = todoItemDom.querySelector('.delete')
   const editButton = todoItemDom.querySelector('.edit');
-  // const titleSpan = todoItemDom.querySelector('span');
-  // const editInput = document.createElement('input');
   // 把原本的title传入input
   const originalTitle = titleSpan.innerText;
-  titleSpan.style.visibility='hidden'
-  editInput.style.visibility="visible"
-  // todoItemDom.replaceChild(editInput, titleSpan);
-  editInput.value = originalTitle;
+  titleSpan.style.visibility = 'hidden'
+  editInput.style.visibility = "visible"
+  editInput.value = `New name for--->${originalTitle}`;
   editInput.classList.add('edit-input')
   todoItemDom.appendChild(editInput)
-//创建取消按钮
-//  const cancelButton=document.createElement('button');
- cancelButton.innerText='Cancel';
- cancelButton.classList.add('cancel');
- todoItemDom.appendChild(cancelButton);
-//创建保存按钮
-
-  // const saveButton = document.createElement('button');
+  //创建取消按钮
+  cancelButton.innerText = 'Cancel';
+  cancelButton.classList.add('cancel');
+  todoItemDom.appendChild(cancelButton);
+  //创建保存按钮
   saveButton.innerText = 'Save';
   saveButton.classList.add('save');
   todoItemDom.appendChild(saveButton);
-  cancelButton.style.display='block'
-  saveButton.style.display='block'
+  cancelButton.style.display = 'block'
+  saveButton.style.display = 'block'
   //隐藏编辑按钮
   editButton.style.display = 'none'
-  deleteButton.style.display='none'
-  // const deleteButton=todoItemDom.querySelector('.delete')
-  //点击编辑按钮后 编辑和删除按钮隐藏
-//   if(cancelButton.style.display='block'){
-//   editButton.style.display = 'none'
-//   deleteButton.style.display='none'
-//   editInput.style.display='block'
-//   saveButton.style.display='block'
-// }else{
-//   editButton.style.display='block'
-//   deleteButton.style.display='block'
-// }
+  deleteButton.style.display = 'none'
   //保存按钮绑定事件
   saveButton.addEventListener('click', () => {
     const newTitle = editInput.value.trim();
@@ -272,45 +224,39 @@ const toggleEditMode = (todoItemDom,titleSpan) => {
         .then(() => {
           updateTodoDomTitle(todoItemDom, newTitle);
           editButton.style.display = 'inline-block';
-          toggleEditModeOff( originalTitle);
+          toggleEditModeOff(originalTitle);
         })
         .catch((error) => console.error(error.message));
     } else {
       alert('Title cannot be empty!');
-    }
+    } location.reload()
   })
 
 };
-//点击取消按钮后 隐藏输入框和两个两个按钮  编辑和删除按钮恢复
-cancelButton.addEventListener('click',()=>{
-  toggleEditModeOff( );
+//点击取消按钮后 进入取消编辑模式
+cancelButton.addEventListener('click', (event) => {
+  toggleEditModeOff(event);
 })
-//取消编辑模式
-const toggleEditModeOff = ( originalTitle,todoItemDom) => {
-  // const editButton = todoItemDom.querySelector('.edit');
-  // const deleteButton = todoItemDom.querySelector('.delete');
-  const deleteButton=document.querySelector('.delete')
-  const editButton = document.querySelector('.edit');
-  // const titleSpan = document.querySelector('span');
-  // 移除并恢复标题
-// todoItemDom=document.querySelector('li')
-//   const titleSpan = document.createElement('span');
-//   titleSpan.innerText = originalTitle;
-//   todoItemDom.replaceChild(titleSpan,editInput );
-const titleSpan = document.querySelector('span');
-titleSpan.style.visibility="visible"
-editInput.style.visibility="hidden"
-  // todoItemDom.appendChild(titleSpan);
-// titleSpan.style.display='block'
-  // 隐藏输入框和按钮
-  // editInput.style.display = 'none';
+//7.取消编辑模式，恢复文本和eidt，delete按钮
+const toggleEditModeOff = (event) => {
+  const clickedButton = event.target;
+  const parentLi = clickedButton.closest('li');
+  const deleteButton = parentLi.querySelector('.delete');
+  const editButton = parentLi.querySelector('.edit');
+  const titleSpan = parentLi.querySelector('span');
+  titleSpan.style.visibility = "visible";
+  editInput.style.visibility = "hidden";
   cancelButton.style.display = 'none';
   saveButton.style.display = 'none';
-
   // 显示编辑和删除按钮
   editButton.style.display = 'inline-block';
   deleteButton.style.display = 'inline-block';
 };
+
+const Active = document.getElementById('active');
+const completed = document.getElementById('Completed');
+const ALL = document.getElementById('reset');
+
 //8.发送请求更新为新的title
 const updateTodoTitle = (id, newTitle) => {
   return updateTodo(id, { title: newTitle })
@@ -322,74 +268,24 @@ const updateTodoTitle = (id, newTitle) => {
       console.error(error.message);
     });
 };
-
-const Active = document.getElementById('active');
-const completed = document.getElementById('Completed');
-const ALL=document.getElementById('reset')
+//9.查询代办事项和已经完成的事项
 //在进行的
 Active.addEventListener('click', Activetodo)
-
 function Activetodo() {
-  const remain = document.getElementById("remain");
-  const done=document.querySelectorAll('li[data-status="0"]')
-
-  const remains = (`${done.length}tasks activing`)
-  remain.innerHTML = remains
-  let lis = document.querySelectorAll('li')
-  lis.forEach(lis => {
-    if (lis.dataset.status === '1') {
-      lis.style.display = 'none';
-    } else {
-      lis.style.display = 'block';
-
-    }
-  })
-  
+  tab = 'active';
+  getTodosAndRender(tab)
 }
-
 //完成的
 completed.addEventListener('click', completedtodo)
-function completedtodo() {
-  const remain = document.getElementById("remain");
-  const done=document.querySelectorAll('li[data-status="1"]')
-  const remains = (`${done.length}tasks Complted`)
-  remain.innerHTML = remains
-  let lis = document.querySelectorAll('li');
-  lis.forEach(lis => {
-    if (lis.dataset.status==='1') {
-
-      lis.style.display = 'block';
-    } else {
-      lis.style.display = 'none';
-    }
-  })
-//   const  checkbox=document.querySelector('.div1')
-//   checkbox.addEventListener('click', (id,status) => {
-//     const status = checkbox.checked ? 1 : 0;
-//     updateTodoStatus(id, status)
-//       .then(() => {
-//         // todoItemDom.classList.toggle('status', status === 1);
-//         todoItemDom.dataset.status = status;
-//         console.log('Todo updated successfully:');
-//         todoItemDom.remove()
-//       })
-//       getLength(completed)
-// })
-
+function completedtodo(status) {
+  tab = 'completed'
+  getTodosAndRender(tab)
 }
 //全部的 
-ALL.addEventListener('click',Alltodo)
-
-function Alltodo(){
-  const ALL=document.getElementById('reset')
-  ALL.setAttribute('aria-expanded','true')
-  let lis = document.querySelectorAll('li');
-  lis.forEach(lis => {
-    lis.style.display = "block"
-})
-const remain = document.getElementById("remain");
-  const remains = (`${lis.length}tasks remaining`)
-  remain.innerHTML = remains
+ALL.addEventListener('click', Alltodo)
+function Alltodo() {
+  tab = 'all';
+  getTodosAndRender(tab)
 }
 // 执行
 function main() {
@@ -398,9 +294,7 @@ function main() {
   // 事件添加
   const addButton = document.getElementById("todo-button");
   addButton.addEventListener("click", addTodo, false);
-
-
-
+  //如果把事件都写在这代码简洁又精练，但能力不够只能先按照自己的写 
 }
 
 // 浏览器资源加载完成执行
